6d185d8d4c1ac9fc8bf5c65f01fd15b0b55e6d06
Brian Surowiec <brian@13degrees.com>
2021-04-16 20:46:04 -0400

    Refactor attribute verification

--- test/IconTagHelperTests.cs
+++ test/IconTagHelperTests.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -29,8 +30,8 @@
             // Then
             output.TagMode.ShouldBe(TagMode.StartTagAndEndTag);
             output.TagName.ShouldBe("svg");
-            output.Attributes.ShouldContain(new TagHelperAttribute("aria-hidden", "true"));
-            output.Attributes.ShouldContain(new TagHelperAttribute("viewbox", "0 0 24 24"));
+            AssertAttributeValue(output.Attributes, "aria-hidden", "true");
+            AssertAttributeValue(output.Attributes, "viewbox", "0 0 24 24");
         }
 
         [Theory]
@@ -46,16 +47,16 @@
             var options = Options.Create(new HeroiconOptions());
             var helper = new IconTagHelper(options)
             {
-                Icon = IconSymbol.Bell
+                Icon = IconSymbol.Bell,
             };
 
             // When
             helper.Process(context, output);
 
             // Then
-            output.Attributes.ShouldContain(new TagHelperAttribute("fill", "none"));
-            output.Attributes.ShouldContain(new TagHelperAttribute("stroke", "currentColor"));
-            output.Attributes.ShouldContain(new TagHelperAttribute("viewbox", "0 0 24 24"));
+            AssertAttributeValue(output.Attributes, "fill", "none");
+            AssertAttributeValue(output.Attributes, "stroke", "currentColor");
+            AssertAttributeValue(output.Attributes, "viewbox", "0 0 24 24");
             output.Content.GetContent().ShouldBe("<path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9\"/>");
         }
 
@@ -72,15 +73,15 @@
             var options = Options.Create(new HeroiconOptions());
             var helper = new IconTagHelper(options)
             {
-                Icon = IconSymbol.Bell
+                Icon = IconSymbol.Bell,
             };
 
             // When
             helper.Process(context, output);
 
             // Then
-            output.Attributes.ShouldContain(new TagHelperAttribute("fill", "currentColor"));
-            output.Attributes.ShouldContain(new TagHelperAttribute("viewbox", "0 0 20 20"));
+            AssertAttributeValue(output.Attributes, "fill", "currentColor");
+            AssertAttributeValue(output.Attributes, "viewbox", "0 0 20 20");
             output.Attributes.ShouldNotContain(a => a.Name == "stroke");
             output.Content.GetContent().ShouldBe("<path d=\"M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z\"/>");
         }
@@ -108,14 +109,14 @@
             var options = Options.Create(new HeroiconOptions());
             var helper = new IconTagHelper(options)
             {
-                Icon = IconSymbol.Bell
+                Icon = IconSymbol.Bell,
             };
 
             // When
             helper.Process(context, output);
 
             // Then
-            output.Attributes.ShouldContain(new TagHelperAttribute(attributeName, attributeValue));
+            AssertAttributeValue(output.Attributes, attributeName, attributeValue);
         }
 
         [Fact]
@@ -183,6 +184,15 @@
                 tagName,
                 attributes: attributes,
                 getChildContentAsync: (useCachedResult, encoder) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+        }
+
+        private static void AssertAttributeValue(TagHelperAttributeList attributes, string name, string value)
+        {
+            attributes
+                .Count(a => a.Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                .ShouldBe(1);
+
+            attributes[name].Value.ShouldBe(value);
         }
     }
 }
